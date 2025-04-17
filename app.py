66c9dbd754724cfc9d14b2cd40a064e1c8eff04f
CigoliniMat <matteocigolin@gmail.com>
2025-04-17 21:22:40 +0200

    Work with flask and html

--- app.py
+++ app.py
@@ -3,18 +3,19 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
-def mostra_lista():
-    oggetti = [
-        {'nome': 'Mela', 'prezzo': 1.2},
-        {'nome': 'Banana', 'prezzo': 0.8},
-        {'nome': 'Arancia', 'prezzo': 1.0}
+def show_list():
+    locations_list = [
+        {'ID':1,'name': 'Orzivecchi (BS)', 'lat': 10.2555, 'lon':45.4555},
+        {'ID':2,'name': 'Settimo Torinese (TO)', 'lat': 10.2555, 'lon':45.4555},
+        {'ID':3,'name': 'Dolcè (VR)', 'lat': 10.2555, 'lon':45.4555}
     ]
-    return render_template('lista.html', oggetti=oggetti)
+    return render_template('list.html', locations_list=locations_list)
 
-@app.route('/seleziona', methods=['POST'])
-def seleziona_oggetti():
-    selezionati = request.form.getlist('selezionati')
-    return f"Hai selezionato: {', '.join(selezionati)}" if selezionati else "Non hai selezionato nulla."
+@app.route('/select_location', methods=['POST'])
+def select_location():
+    selection = request.form.getlist('selection')[0]
+    selection = int(selection)
+    return f"you select location: {selection}" if selection else "you don't select any location"
 
 if __name__ == '__main__':
     app.run(debug=True)
