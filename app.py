31388ca904a144067a5dfe1340ab9c8724035240
CigoliniMat <matteocigolin@gmail.com>
2025-04-20 13:57:42 +0200

    Create add location page

--- app.py
+++ app.py
@@ -8,15 +8,18 @@ def show_list():
         {'ID':1,'name': 'Orzivecchi (BS)', 'lat': 10.2555, 'lon':45.4555},
         {'ID':2,'name': 'Settimo Torinese (TO)', 'lat': 10.2555, 'lon':45.4555},
         {'ID':3,'name': 'Dolcè (VR)', 'lat': 10.2555, 'lon':45.4555},
-        {'ID':4,'name': 'Settimo Torinese (TO)', 'lat': 10.2555, 'lon':45.4555},
-    ]
+    ]*20
     return render_template('list.html', locations_list=locations_list)
 
 @app.route('/select_location', methods=['POST'])
 def select_location():
-    selection = request.form.getlist('selection')[0]
-    selection = int(selection)
-    return f"you select location: {selection}" if selection else "you don't select any location"
+    selection = request.form.getlist('selection')
+    if len(selection) > 0:
+        selection = selection[0]
+        selection = int(selection)
+        return f"you select location: {selection}"
+    else:
+        return "you don't select any location"
 
 if __name__ == '__main__':
     app.run(debug=True)
